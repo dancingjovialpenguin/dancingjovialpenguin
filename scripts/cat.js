ade274278e1ffb96ed5308f0ce026764e92d9417
erenoguzyesil <jfv.eoy@gmail.com>
2025-06-02 20:56:13 +0300

    Updated content.

--- scripts/cat.js
+++ scripts/cat.js
@@ -5,9 +5,11 @@ let catStopped = false;
 cat.addEventListener("click", () => {
   if (catStopped) {
     cat.style.animationPlayState = "running";
+    cat.style.opacity = 1;
     catStopped = false;
   } else {
     cat.style.animationPlayState = "paused";
+    cat.style.opacity = 0.5;
     catStopped = true;
   }
 });
