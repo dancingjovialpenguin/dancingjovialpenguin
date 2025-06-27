7927396e857c72bace1c6a12e450a3df55910cfb
erenoguzyesil <jfv.eoy@gmail.com>
2025-06-27 03:43:43 +0300

    Updated styles and scripts.

--- scripts/images.js
+++ scripts/images.js
@@ -1,11 +1,20 @@
+const sidebarDiv = document.getElementById("sidebar");
 const aboutSectionMainTextDiv = document.getElementById("main-text");
 const imagesUl = document.getElementById("images");
 
-let screenWidth = window.innerWidth;
+function placeImages() {
+  let screenWidth = window.innerWidth;
 
-if (screenWidth <= 480) {
-  aboutSectionMainTextDiv.insertBefore(
-    imagesUl,
-    aboutSectionMainTextDiv.children[2]
-  );
+  if (screenWidth <= 750) {
+    aboutSectionMainTextDiv.insertBefore(
+      imagesUl,
+      aboutSectionMainTextDiv.children[2]
+    );
+  } else {
+    sidebarDiv.appendChild(imagesUl);
+  }
 }
+
+placeImages();
+
+window.addEventListener("resize", placeImages);
